bb625113034edb5088f118b9fcabeabaeb9ff327
GalvinPython <77013913+GalvinPython@users.noreply.github.com>
2024-02-15 15:53:34 +0000

    i should stop overcomplicating things

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = 7003;
-
-const apiProxy = createProxyMiddleware({
-    target: 'https://choccycounts.com/api/messages',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/.*': '',
-    },
-    onProxyRes: (proxyRes, req, res) => {
-        console.log('Request made!')
-        try {
-            // Change the content type header to application/json
-            proxyRes.headers['content-type'] = 'application/json';
-        } catch (error) {
-            console.error('Error modifying response headers:', error);
-        }
-    },
-});
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
     next();
 });
 
-
-app.use('*', apiProxy);
+app.get('*', async(req, res) => {
+    return res.send(await fetch('https://choccycounts.com/api/messages').then(resp=>resp.json()).catch(console.error))
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
